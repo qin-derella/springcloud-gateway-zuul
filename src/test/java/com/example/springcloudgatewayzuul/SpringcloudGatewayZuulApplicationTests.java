999cfd00b86797422b629f97a9c6b6bc8ecaf26a
刘月芹 <liuyueqin@17of.com>
2020-08-19 14:46:46 +0800

    服务网关路由

---
+++ src/test/java/com/example/springcloudgatewayzuul/SpringcloudGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudgatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudGatewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
