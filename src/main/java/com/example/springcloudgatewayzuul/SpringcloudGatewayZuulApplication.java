e5227ded39418cb349e75411272807cacbdfe69d
刘月芹 <liuyueqin@17of.com>
2020-08-19 15:12:40 +0800

    zuul过滤器

--- src/main/java/com/example/springcloudgatewayzuul/SpringcloudGatewayZuulApplication.java
+++ src/main/java/com/example/springcloudgatewayzuul/SpringcloudGatewayZuulApplication.java
@@ -1,9 +1,12 @@
 package com.example.springcloudgatewayzuul;
 
+import com.example.springcloudgatewayzuul.filter.MyFilter;
+import com.fasterxml.jackson.core.filter.TokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -12,6 +15,12 @@ public class SpringcloudGatewayZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudGatewayZuulApplication.class, args);
+    }
+
+
+    @Bean
+    public MyFilter myFilter() {
+        return new MyFilter();
     }
 
 }
